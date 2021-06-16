4d61b59c7f80a6fdc787d1f2829e7fc01aee7808
Jiahao XU <Jiahao_XU@outlook.com>
2021-06-16 18:10:10 +1000

    Declare syscall::wrapper::Fd
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/syscall.rs
+++ src/syscall.rs
@@ -2,4 +2,23 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+pub use std::os::raw::c_int;
+
 include!(concat!(env!("OUT_DIR"), "/syscall_binding.rs"));
+
+pub mod wrapper {
+    use std::os::raw::c_int;
+
+    pub struct Fd {
+        fd: c_int,
+    }
+    impl Fd {
+        pub fn new(fd: c_int) -> Result<Fd, ()> {
+            if fd >= 0 {
+                Ok(Fd {fd})
+            } else {
+                Err(())
+            }
+        }
+    }
+}
