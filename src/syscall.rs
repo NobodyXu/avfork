14d8a96194a0cdd8b0d2d0f144302089b03a64f7
Jiahao XU <Jiahao_XU@outlook.com>
2021-06-18 16:12:52 +1000

    Convert Fd to an non-owning type and Add new RAII type FdBox
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/syscall.rs
+++ src/syscall.rs
@@ -6,10 +6,35 @@ mod binding {
     include!(concat!(env!("OUT_DIR"), "/syscall_binding.rs"));
 }
 
-pub use binding::{sigset_t, pid_t};
+use std::ops::Deref;
 use std::os::raw::{c_void, c_int};
 
+pub use binding::{sigset_t, pid_t};
+
 use crate::error::{toResult, SyscallError};
+
+pub struct FdBox {
+    fd: Fd,
+}
+impl FdBox {
+    pub fn from_raw(fd: c_int) -> FdBox {
+        FdBox { fd: Fd{fd} }
+    }
+}
+impl Drop for FdBox {
+    fn drop(&mut self) {
+        unsafe {
+            binding::psys_close(self.fd.fd);
+        }
+    }
+}
+impl Deref for FdBox {
+    type Target = Fd;
+
+    fn deref(&self) ->&Self::Target {
+        &self.fd
+    }
+}
 
 pub struct Fd {
     fd: c_int,
@@ -23,13 +48,6 @@ impl Fd {
         let buf_ptr = buffer.as_mut_ptr() as *mut c_void;
         let buf_len = buffer.len() as u64;
         Ok(toResult(unsafe { binding::psys_read(self.fd, buf_ptr, buf_len) })? as usize)
-    }
-}
-impl Drop for Fd {
-    fn drop(&mut self) {
-        unsafe {
-            binding::psys_close(self.fd);
-        }
     }
 }
 
