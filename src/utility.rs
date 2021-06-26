37c9fde8e4c4279bb3bc53110e90bc93d9d5c3db
Jiahao XU <Jiahao_XU@outlook.com>
2021-06-26 21:39:43 +1000

    Fix utility::to_void_ptr and add utility::to_void_ptr_mut
    Fix the former to return *const c_void while add the later to
    return *mut c_void for &mut T
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/utility.rs
+++ src/utility.rs
@@ -1,5 +1,9 @@
 use std::os::raw::c_void;
 
-pub fn to_void_ptr<T>(reference: &T) -> *mut c_void {
-    reference as *const _ as *mut c_void
+pub fn to_void_ptr<T>(reference: &T) -> *const c_void {
+    reference as *const _ as *const c_void
+}
+
+pub fn to_void_ptr_mut<T>(reference: &mut T) -> *mut c_void {
+    reference as *mut _ as *mut c_void
 }
