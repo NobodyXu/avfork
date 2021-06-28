9a656e41fa3f4321032ae31c07a1f3f6358c2c49
Jiahao XU <Jiahao_XU@outlook.com>
2021-06-28 21:48:32 +1000

    Improve doc on mod syscall and mod lowlevel
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 /// rust bindings for aspawn.h, generated using rust-bindgen
 pub mod aspawn;
 /// rust bindings for syscall.h, generated using rust-bindgen
+/// **ALL FUNCTIONS IN THIS MODULE IS SAFE TO BE USED INSIDE THE CALLBACK OF `avfork`
+/// or `avforkrec`**
 pub mod syscall;
 
 /// utilty functions used in this library
@@ -9,7 +11,7 @@ pub mod utility;
 /// wrapper for errno_msg and provide an easy-to-use interface
 pub mod error;
 
-/// lowlevel wrapper of aspawn and syscall
+/// lowlevel wrapper of aspawn
 pub mod lowlevel;
 
 extern crate once_cell;
